a4db64b45b5ad5713176e0b9b06c933a17561085
redlac <runeman2@gmail.com>
2020-05-06 21:08:28 -0600

    Add client/build folder to git

---
+++ client/build/precache-manifest.86f2be0768dee0af8a7333042bb3be79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1997f5708cff9bfff389d5e506f329d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "251a4e1ca6f782363a7a",
+    "url": "/static/css/main.0c104f6b.chunk.css"
+  },
+  {
+    "revision": "ab1ba534e481f6fa5a2c",
+    "url": "/static/js/2.75a798f6.chunk.js"
+  },
+  {
+    "revision": "3be25bf51018a491ef7f73dfd2dd74c4",
+    "url": "/static/js/2.75a798f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "251a4e1ca6f782363a7a",
+    "url": "/static/js/main.e98b94c1.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  }
+]);
